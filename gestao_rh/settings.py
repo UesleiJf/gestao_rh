fcba049d55f2312a514eb24f33d2477232a7b20c
ueslei <uesleijf@gmail.com>
2019-03-11 09:50:09 -0300

    Home generic, submodules bootstrap

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'apps.departamentos',
     'apps.documentos',
     'apps.registro_hora_extra',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
