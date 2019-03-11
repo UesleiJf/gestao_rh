3af1740967e60ea398c4c1982297a6ca6e999255
ueslei <uesleijf@gmail.com>
2019-03-11 16:29:19 -0300

    Crud part 3 - Update Funcionario/Empresa

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+from django.urls import reverse
+
+
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome da Empresa')
 
     def __str__(self):
         return self.nome
+
+    def get_absolute_url(self):
+        return reverse('home')
 
