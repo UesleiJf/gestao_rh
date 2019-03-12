77ef71bbe6716e2dff1644d0498cd3d2bb15ae25
ueslei <uesleijf@gmail.com>
2019-03-12 12:06:56 -0300

    CRUD Departamentos - Update

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import DepartamentosList
+from .views import (
+    DepartamentoCreate,
+    DepartamentoUpdate,
+    DepartamentoDelete,
+    DepartamentosList,
+)
 
 
 urlpatterns = [
-    path('list', DepartamentosList.as_view(), name='list_departamentos'),
-
+    path('', DepartamentosList.as_view(), name='list_departamentos'),
+    path('novo', DepartamentoCreate.as_view(), name='create_departamentos'),
+    path('update/<int:pk>', DepartamentoUpdate.as_view(), name='update_departamento'),
+    path('delete/<int:pk>', DepartamentoDelete.as_view(), name='delete_departamento'),
 ]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,4 +1,10 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Departamento
 
 
@@ -8,3 +14,26 @@ class DepartamentosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Departamento.objects.filter(empresa=empresa_logada)
+
+
+class DepartamentoCreate(CreateView):
+    model = Departamento
+    fields = ['nome']
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)
+
+
+class DepartamentoUpdate(UpdateView):
+    model = Departamento
+    fields = ['nome']
+
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    fields = ['nome']
+    success_url = reverse_lazy('list_departamentos')
+
