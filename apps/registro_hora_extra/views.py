4402f3027f64830d14ed37d3b72c45e286df4344
ueslei <uesleijf@gmail.com>
2019-03-12 21:22:24 -0300

    CRUD Delete horas extras

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView
 from .models import RegistroHoraExtra
 
 
@@ -10,6 +10,13 @@ class HoraExtraListList(ListView):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
 
+
 class HoraExtraEdit(UpdateView):
     model = RegistroHoraExtra
     fields = ['motivo', 'funcionario', 'horas']
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
